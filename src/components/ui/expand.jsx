0ebfd38d6f2b653cb8f6611b5149dde8cadc0ce9
karthik <karthik.nishanth06@gmail.com>
2024-11-21 15:43:27 +0000

    team

--- src/components/ui/expand.jsx
+++ src/components/ui/expand.jsx
@@ -7,7 +7,7 @@ import {
   ExpandableCardContent,
   ExpandableTrigger,
 } from "@/components/ui/expandable";
-
+import { Button } from "@/components/ui/button";
 export default function ExpandableComponent({
   imageSrc,
   imageAlt,
@@ -34,56 +34,66 @@ export default function ExpandableComponent({
       onExpandStart={onExpandStart}
       onExpandEnd={onExpandEnd}
     >
-      {({ isExpanded }) => (
-        <ExpandableTrigger>
-          <ExpandableCard
-            className="w-full relative"
-            collapsedSize={collapsedSize}
-            expandedSize={expandedSize}
-            hoverToExpand={hoverToExpand}
-            expandDelay={expandDelay}
-            collapseDelay={collapseDelay}
-          >
-            <ExpandableCardContent>
-              <div className="flex flex-col items-center">
-                {imageSrc && (
-                  <img
-                    src={imageSrc}
-                    alt={imageAlt || "Expandable content"}
-                    className="object-cover rounded-md mb-4"
-                    style={{
-                      width: isExpanded
-                        ? expandedImageSize.width
-                        : collapsedImageSize.width,
-                      height: isExpanded
-                        ? expandedImageSize.height
-                        : collapsedImageSize.height,
-                      transition: "width 0.3s, height 0.3s",
-                    }}
-                  />
-                )}
-                <div className="text-center">
-                  {title && (
-                    <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      {({ isExpanded, toggleExpand }) => (
+        <>
+          <ExpandableTrigger>
+            <ExpandableCard
+              className={`w-full relative ${isExpanded ? "z-10" : "z-0"}`}
+              collapsedSize={collapsedSize}
+              expandedSize={expandedSize}
+              hoverToExpand={hoverToExpand}
+              expandDelay={expandDelay}
+              collapseDelay={collapseDelay}
+            >
+              <ExpandableCardContent>
+                <div className="flex flex-col items-center">
+                  {imageSrc && (
+                    <img
+                      src={imageSrc}
+                      alt={imageAlt || "Expandable content"}
+                      className="object-cover rounded-md mb-4"
+                      style={{
+                        width: isExpanded
+                          ? expandedImageSize.width
+                          : collapsedImageSize.width,
+                        height: isExpanded
+                          ? expandedImageSize.height
+                          : collapsedImageSize.height,
+                        transition: "width 0.3s, height 0.3s",
+                      }}
+                    />
                   )}
-                  {position && (
-                    <p className="text-sm text-gray-500 mb-2">{position}</p>
-                  )}
-                  {isExpanded && (
-                    <>
-                      {description && (
-                        <p className="text-sm text-gray-600 dark:text-gray-400">
-                          {description}
-                        </p>
-                      )}
-                      {children}
-                    </>
-                  )}
+                  <div className="text-center">
+                    {title && (
+                      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+                    )}
+                    {position && (
+                      <p className="text-sm text-gray-500 mb-2">{position}</p>
+                    )}
+                    {isExpanded && (
+                      <>
+                        {description && (
+                          <p className="text-sm text-gray-600 dark:text-gray-400">
+                            {description}
+                          </p>
+                        )}
+                        {children}
+                      </>
+                    )}
+                  </div>
+                  <Button
+                    onClick={toggleExpand}
+                    className={`mb-2 px-4 py-2  text-white rounded  transition-colors justify-center items-center ${
+                      !isExpanded ? "mt-0" : "mt-10"
+                    }`}
+                  >
+                    {isExpanded ? "Show Less" : "More Info"}
+                  </Button>
                 </div>
-              </div>
-            </ExpandableCardContent>
-          </ExpandableCard>
-        </ExpandableTrigger>
+              </ExpandableCardContent>
+            </ExpandableCard>
+          </ExpandableTrigger>
+        </>
       )}
     </Expandable>
   );
